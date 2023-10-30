240f02d0b08bd70404d0daaa8ab71793a041a4d4
seblex9 <seblex@proton.me>
2023-10-30 12:09:46 +0800

    Include CORS module; Remove node_modules from Git tracking

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cors from 'cors';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
