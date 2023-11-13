e08a2ec95cd0a28b4f231b5212378c6025f99d0c
seblex9 <seblex@proton.me>
2023-11-13 17:31:27 +0800

    Combine signup into single controller action and one route

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,4 +1,5 @@
 import Member from '../models/memberModel.js';
+import User from '../models/userModel.js';
 
 const createOrUpdateMember = async (userId, updateData) => {
   const member = await Member.findOneAndUpdate(
@@ -9,115 +10,58 @@ const createOrUpdateMember = async (userId, updateData) => {
   return member;
 };
 
-export const handleForeignStatus = async (req, res) => {
-  try {
-    const member = await createOrUpdateMember(req.body.userId, {
-      isForeignPassportHolder: req.body.isForeignPassportHolder,
-    });
-    res.status(200).json({
-      message: 'Foreign status recorded. Proceed to next step.',
-      member,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
+export const handleSignup = async (req, res) => {
+  const updateData = {};
+  const fields = [
+    'isForeignPassportHolder',
+    'name',
+    'email',
+    'birthday',
+    'nationality',
+    'area',
+    'phone',
+    'wechatID',
+    'worksOnWeekdays',
+    'hasChildren',
+    'childrenAgeGroup',
+    'interestedActivities',
+    'leadingActivitiesInterest',
+    'learnedAboutSWIC',
+    'feedback',
+    'acceptedTermsAndConditions',
+  ];
 
-export const handleDetails = async (req, res) => {
-  try {
-    const {
-      userId,
-      name,
-      email,
-      birthday,
-      nationality,
-      area,
-      phone,
-      wechatID,
-    } = req.body;
-    const member = await createOrUpdateMember(userId, {
-      name,
-      email,
-      birthday,
-      nationality,
-      area,
-      phone,
-      wechatID,
-    });
-    res
-      .status(200)
-      .json({ message: 'Details recorded. Proceed to next step.', member });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
-
-export const handleAvailability = async (req, res) => {
-  try {
-    const member = await createOrUpdateMember(req.body.userId, {
-      worksOnWeekdays: req.body.worksOnWeekdays,
-      hasChildren: req.body.hasChildren,
-      childrenAgeGroup: req.body.childrenAgeGroup,
-    });
-    res.status(200).json({
-      message: 'Availability data saved. Proceed to next step.',
-      member,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
-
-export const handleInterests = async (req, res) => {
-  try {
-    const member = await createOrUpdateMember(req.body.userId, {
-      interestedActivities: req.body.interestedActivities,
-    });
-    res
-      .status(200)
-      .json({ message: 'Interests data saved. Proceed to next step.', member });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
-
-export const handleLeadActivity = async (req, res) => {
-  try {
-    const member = await createOrUpdateMember(req.body.userId, {
-      leadingActivitiesInterest: req.body.leadingActivitiesInterest,
-    });
-    res.status(200).json({
-      message: 'Lead activity data saved. Proceed to next step.',
-      member,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
-
-export const handleLearnedAbout = async (req, res) => {
-  try {
-    const { userId, learnedAboutSWIC } = req.body;
-
-    if (
-      !learnedAboutSWIC ||
-      !['Online search', 'Friends', 'WeChat', 'Facebook', 'Other'].includes(
-        learnedAboutSWIC
-      )
-    ) {
-      return res
-        .status(400)
-        .json({ message: 'Invalid value for learnedAboutSWIC.' });
+  // Collect data from the request body
+  fields.forEach(field => {
+    if (req.body.hasOwnProperty(field)) {
+      updateData[field] = req.body[field];
     }
+  });
 
-    const member = await createOrUpdateMember(userId, { learnedAboutSWIC });
+  // Validate 'learnedAboutSWIC' if it exists
+  if (
+    updateData.learnedAboutSWIC &&
+    !['Online search', 'Friends', 'WeChat', 'Facebook', 'Other'].includes(
+      updateData.learnedAboutSWIC
+    )
+  ) {
+    return res
+      .status(400)
+      .json({ message: 'Invalid value for learnedAboutSWIC.' });
+  }
+
+  // Check if terms are accepted if present
+  if (
+    updateData.hasOwnProperty('acceptedTermsAndConditions') &&
+    !updateData.acceptedTermsAndConditions
+  ) {
+    return res.status(400).json({ message: 'Terms must be accepted.' });
+  }
+
+  try {
+    const member = await createOrUpdateMember(req.body.userId, updateData);
     res.status(200).json({
-      message: 'Information on how you learned about SWIC has been recorded.',
+      message: 'Signup process complete. All provided data has been recorded.',
       member,
     });
   } catch (error) {
@@ -126,35 +70,163 @@ export const handleLearnedAbout = async (req, res) => {
   }
 };
 
-export const handleFeedback = async (req, res) => {
-  try {
-    const member = await createOrUpdateMember(req.body.userId, {
-      feedback: req.body.feedback,
-    });
-    res.status(200).json({
-      message: 'Feedback recorded. Proceed to agree to terms and conditions.',
-      member,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
+// import Member from '../models/memberModel.js';
 
-export const handleTerms = async (req, res) => {
-  try {
-    const { userId, acceptedTermsAndConditions } = req.body;
-    const member = await createOrUpdateMember(userId, {
-      acceptedTermsAndConditions,
-    });
-    if (!acceptedTermsAndConditions) {
-      return res.status(400).json({ message: 'Terms must be accepted.' });
-    }
-    res
-      .status(200)
-      .json({ message: 'Terms accepted. Registration complete.', member });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server Error.' });
-  }
-};
+// const createOrUpdateMember = async (userId, updateData) => {
+//   const member = await Member.findOneAndUpdate(
+//     { userAccount: userId },
+//     updateData,
+//     { new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true }
+//   );
+//   return member;
+// };
+
+// export const handleForeignStatus = async (req, res) => {
+//   try {
+//     const member = await createOrUpdateMember(req.body.userId, {
+//       isForeignPassportHolder: req.body.isForeignPassportHolder,
+//     });
+//     res.status(200).json({
+//       message: 'Foreign status recorded. Proceed to next step.',
+//       member,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleDetails = async (req, res) => {
+//   try {
+//     const {
+//       userId,
+//       name,
+//       email,
+//       birthday,
+//       nationality,
+//       area,
+//       phone,
+//       wechatID,
+//     } = req.body;
+//     const member = await createOrUpdateMember(userId, {
+//       name,
+//       email,
+//       birthday,
+//       nationality,
+//       area,
+//       phone,
+//       wechatID,
+//     });
+//     res
+//       .status(200)
+//       .json({ message: 'Details recorded. Proceed to next step.', member });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleAvailability = async (req, res) => {
+//   try {
+//     const member = await createOrUpdateMember(req.body.userId, {
+//       worksOnWeekdays: req.body.worksOnWeekdays,
+//       hasChildren: req.body.hasChildren,
+//       childrenAgeGroup: req.body.childrenAgeGroup,
+//     });
+//     res.status(200).json({
+//       message: 'Availability data saved. Proceed to next step.',
+//       member,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleInterests = async (req, res) => {
+//   try {
+//     const member = await createOrUpdateMember(req.body.userId, {
+//       interestedActivities: req.body.interestedActivities,
+//     });
+//     res
+//       .status(200)
+//       .json({ message: 'Interests data saved. Proceed to next step.', member });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleLeadActivity = async (req, res) => {
+//   try {
+//     const member = await createOrUpdateMember(req.body.userId, {
+//       leadingActivitiesInterest: req.body.leadingActivitiesInterest,
+//     });
+//     res.status(200).json({
+//       message: 'Lead activity data saved. Proceed to next step.',
+//       member,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleLearnedAbout = async (req, res) => {
+//   try {
+//     const { userId, learnedAboutSWIC } = req.body;
+
+//     if (
+//       !learnedAboutSWIC ||
+//       !['Online search', 'Friends', 'WeChat', 'Facebook', 'Other'].includes(
+//         learnedAboutSWIC
+//       )
+//     ) {
+//       return res
+//         .status(400)
+//         .json({ message: 'Invalid value for learnedAboutSWIC.' });
+//     }
+
+//     const member = await createOrUpdateMember(userId, { learnedAboutSWIC });
+//     res.status(200).json({
+//       message: 'Information on how you learned about SWIC has been recorded.',
+//       member,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleFeedback = async (req, res) => {
+//   try {
+//     const member = await createOrUpdateMember(req.body.userId, {
+//       feedback: req.body.feedback,
+//     });
+//     res.status(200).json({
+//       message: 'Feedback recorded. Proceed to agree to terms and conditions.',
+//       member,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
+
+// export const handleTerms = async (req, res) => {
+//   try {
+//     const { userId, acceptedTermsAndConditions } = req.body;
+//     const member = await createOrUpdateMember(userId, {
+//       acceptedTermsAndConditions,
+//     });
+//     if (!acceptedTermsAndConditions) {
+//       return res.status(400).json({ message: 'Terms must be accepted.' });
+//     }
+//     res
+//       .status(200)
+//       .json({ message: 'Terms accepted. Registration complete.', member });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Server Error.' });
+//   }
+// };
--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,24 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import {
-  handleForeignStatus,
-  handleDetails,
-  handleAvailability,
-  handleInterests,
-  handleLeadActivity,
-  handleLearnedAbout,
-  handleFeedback,
-  handleTerms,
-} from '../controllers/memberController.js';
+import { handleSignup } from '../controllers/memberController.js';
 
 // These routes are unsecured for development
-router.route('/foreignStatus').post(handleForeignStatus);
-router.route('/details').post(handleDetails);
-router.route('/availability').post(handleAvailability);
-router.route('/interests').post(handleInterests);
-router.route('/leadActivity').post(handleLeadActivity);
-router.route('/learnedAbout').post(handleLearnedAbout);
-router.route('/feedback').post(handleFeedback);
-router.route('/terms').post(handleTerms);
+router.route('/signup').post(handleSignup);
 
 export default router;
