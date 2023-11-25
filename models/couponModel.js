53d3bb2c16064b2cf2fae7ff3af43976075e84d5
seblex9 <seblex@proton.me>
2023-11-25 13:08:14 +0800

    Modify coupon model

--- models/couponModel.js
+++ models/couponModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const couponSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  title: { type: String, required: true },
   description: { type: String, required: true },
   code: { type: String, required: true },
   discount: { type: Number, required: true },
